7139d045f2a478d5ac88151e0c3ea5f129e73e3e
Mike Owens <mike.owens@fastmail.com>
2022-10-17 06:26:17 -0400

    format cleanup, add example

--- shellScripts/snippet_confirmPackageInstalled.sh
+++ shellScripts/snippet_confirmPackageInstalled.sh
@@ -1,10 +1,13 @@
 # Function to confirm a package is installed on the system (using dpkg)
 # Exit code is 0 for success, 1 for failure
 
-confirmPackageInstalled() {
-	if [[ $(dpkg -l | grep -c $1) -gt 0 ]]; then
-		return 0
-	else
-		return 1
-	fi
-}
+confirmPackageInstalled() { if [[ $(dpkg -l | grep -c $1) -gt 0 ]]; then return 0; else return 1; fi }
+
+# Example Usage:
+# confirmPackageInstalled myPackage
+# status=$?
+# if [[ $status -eq 0 ]]; then
+# 	do something
+# else
+# 	do something else
+# fi
